63633edfe0be72cf7dc8e4ce55d58f69832abc42
Brandon <bwinkler4@gmail.com>
2023-07-22 23:50:24 -0700

    successfully connected to local database

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
 
 async function bootstrap() {
+  // load env variables
+  config();
+  
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
