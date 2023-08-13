e5dced9e5dbe5fa019f02306a0c4396889379176
Brandon <bwinkler4@gmail.com>
2023-08-12 17:32:40 -1000

    set up aws credentials for front end and installed aws-sdk on frontend

--- frontend/src/components/UploadPhotos.tsx
+++ frontend/src/components/UploadPhotos.tsx
@@ -1,4 +1,6 @@
 import { useState, ChangeEvent } from "react";
+import * as AWS from 'aws-sdk';
+import 'dotenv/config'
 
 const UploadPhotos: React.FC = () => {
 
@@ -10,6 +12,14 @@ const UploadPhotos: React.FC = () => {
             setSelectedFiles([...files]);
         }
     };
+
+    AWS.config.update({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
+    });
+
+    const s3 = new AWS.S3();
 
     return (
         <>
