f4eed0c1460b5be5d77b903fce49f2578c54e6a6
Brandon <bwinkler4@gmail.com>
2023-09-22 23:21:45 -0700

    installed express sessions and created the settings for it, will implement it with passport later

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
 import { config } from 'dotenv';
 
 async function bootstrap() {
@@ -7,6 +9,15 @@ async function bootstrap() {
   config();
   
   const app = await NestFactory.create(AppModule);
+  app.use(session({
+      secret: process.env.SECRET_SESSION_KEY,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 60000,
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
